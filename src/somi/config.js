81bf8bef8926bca1a1bd9aeda210d8ab3c4fefec
Aylan Mello <aylanismello@gmail.com>
2026-08-11 01:36:50 -0700

    yeetz

--- src/somi/config.js
+++ src/somi/config.js
@@ -227,7 +227,7 @@ export const FACTS = {
 }
 
 /** Last substantive edit to the legal pages. Bump when you change them. */
-export const LEGAL_UPDATED = 'August 10, 2026'
+export const LEGAL_UPDATED = 'August 11, 2026'
 
 export const NAV = [
   { href: p('/#how-it-works'), label: 'How it works' },
